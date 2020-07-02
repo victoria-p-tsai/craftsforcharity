265669a57cd68ac96b06b2edff8155883066b707
koienshi <55773406+koienshi@users.noreply.github.com>
2020-07-01 21:56:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//header animation: transparent --> white
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 50);
+})
