3f0c7db275c3d28e0f4ba663d02e0eed2bd86844
koienshi <55773406+koienshi@users.noreply.github.com>
2020-07-03 02:52:10 -0400

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-//header animation: transparent --> white
-window.addEventListener("scroll", function() {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 50);
-})
